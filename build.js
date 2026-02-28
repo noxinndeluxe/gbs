7aba5a42468985f2523fd5bb891e4637cdb6d893
noxinndeluxe <alanyahalkhaber@gmail.com>
2026-02-28 12:23:07 +0300

    Update build.js

--- build.js
+++ build.js
@@ -4,7 +4,8 @@ let html = fs.readFileSync('otel-geri-bildirim-sistemi-v5.html', 'utf8');
 
 html = html
   .replace(/%%SUPABASE_URL%%/g, process.env.SUPABASE_URL || '')
-  .replace(/%%SUPABASE_ANON_KEY%%/g, process.env.SUPABASE_ANON_KEY || '');
+  .replace(/%%SUPABASE_ANON_KEY%%/g, process.env.SUPABASE_ANON_KEY || '')
+  .replace(/%%RESEND_API_KEY%%/g, process.env.RESEND_API_KEY || '');
 
 fs.writeFileSync('otel-geri-bildirim-sistemi-v5.html', html);
 console.log('✅ Done');
